d429e31c1658b1239f37cbab10d78c75a88416e6
helloteacher-alt <panyisa20012544@gmail.com>
2021-04-24 16:05:04 +0700

    ข้อ 6

---
+++ ข้อ6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace examGrab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine();
+            string alpha = Console.ReadLine();
+            Console.WriteLine();
+            int Width = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int Height = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int Layer = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(alpha);
+
+            }
+            while (Width < Width);
+        }
+    }
